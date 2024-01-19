f9577abcf0d15f4e10b3d1443afec6a6c4e93e2d
Cosmin Cosan <56337445+Ccosmin23@users.noreply.github.com>
2024-01-19 19:05:00 +0200

    copy lost files

---
+++ ProiectCM/ProiectCM/ProiectCMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProiectCMApp.swift
+//  ProiectCM
+//
+//  Created by Cosmin Cosan - tapptitude on 19.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ProiectCMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
