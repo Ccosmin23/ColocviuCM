4305c4e97b2019b140293b578c37def47968db9a
Cosmin Cosan <56337445+Ccosmin23@users.noreply.github.com>
2024-01-20 20:48:18 +0200

    code cleanup

--- ProiectCM/ProiectCM/Flows/Authentication/Login/LoginViewModel.swift
+++ ProiectCM/ProiectCM/Flows/Authentication/Login/LoginViewModel.swift
@@ -13,38 +13,40 @@ class LoginViewModel: ObservableObject {
     @Published var hidePassword: Bool = true
     @Published var spinnerEnabled: Bool?
     @Published var scrollTarget: Int?
-
+    
     func loginRequest(callback: @escaping (Bool) -> Void) {
         let loginModel = LoginModel(email: self.email, password: self.password)
         DispatchQueue.main.async {
             self.spinnerEnabled = true
         }
-            API.login(requestParametest: loginModel) { [weak self] result in
-                switch result {
-                case .success(let loginResponse):
-                    if loginResponse.status != "success" {
-//                        ErrorDisplay.show(error: loginResponse.message ?? "no message")
-                    } else {
-                        if let token = loginResponse.token, let username = loginResponse.user?.username, let email = loginResponse.user?.email {
-                            Session.saveSession(token: token)
-                            Session.username = username
-                            Session.email = email
-                            self?.getUser() { result in
-                                if case .success(let userModel) = result {
-                                    if userModel.user.isLicenseValid ?? false {
-                                        callback(true)
-                                    } else {
-                                        callback(false)
-                                    }
+        API.login(requestParametest: loginModel) { [weak self] result in
+            switch result {
+            case .success(let loginResponse):
+                if loginResponse.status != "success" {
+                    //                        ErrorDisplay.show(error: loginResponse.message ?? "no message")
+                } else {
+                    if let token = loginResponse.token, let username = loginResponse.user?.username, let email = loginResponse.user?.email {
+                        Session.saveSession(token: token)
+                        Session.username = username
+                        Session.email = email
+                        self?.getUser() { result in
+                            if case .success(let userModel) = result {
+                                if userModel.user.isLicenseValid ?? false {
+                                    callback(true)
+                                } else {
+                                    callback(false)
                                 }
                             }
                         }
                     }
-                case .failure(let error):
-                    print(error.localizedDescription)
                 }
-                self?.spinnerEnabled = false
+                
+            case .failure(let error):
+                print(error.localizedDescription)
             }
+            
+            self?.spinnerEnabled = false
+        }
     }
     
     func getUser(callback: @escaping (Result<GetUserModel>) -> Void) {
@@ -52,7 +54,7 @@ class LoginViewModel: ObservableObject {
             switch result {
             case .success(let userModel):
                 if userModel.status != "success" {
-//                    ErrorDisplay.show(error: userModel.status)
+                    //                    ErrorDisplay.show(error: userModel.status)
                 } else {
                     Session.username = userModel.user.username
                     callback(.success(userModel))
@@ -65,10 +67,10 @@ class LoginViewModel: ObservableObject {
     
     func showAndHidePassword() {
         switch hidePassword {
-            case true:
-                hidePassword = false
-            case false:
-                hidePassword = true
+        case true:
+            hidePassword = false
+        case false:
+            hidePassword = true
         }
     }
     
@@ -88,11 +90,11 @@ class LoginViewModel: ObservableObject {
         }
         return false
     }
-        
+    
     func checkScrollTarget(target: Int) {
         if target == target {
             scrollTarget = nil
         }
     }
-
+    
 }
