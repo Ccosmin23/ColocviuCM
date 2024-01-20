342a8bde381fbf1bfb5540314d3c82fec39d44dc
Cosmin Cosan <56337445+Ccosmin23@users.noreply.github.com>
2024-01-20 20:17:01 +0200

    removed useless colors

--- ProiectCM/ProiectCM/Utils/Color + Extensions.swift
+++ ProiectCM/ProiectCM/Utils/Color + Extensions.swift
@@ -9,14 +9,5 @@ import Foundation
 import SwiftUI
 
 extension Color {
-    static let lightPurple = Color("LightPurple")
-//    static let cerise = Color("Cerise")
-//    static let russianViolet = Color("RussianViolet")
-//    static let heliotropeGray = Color("HeliotropeGray")
-//    static let customGray = Color("Gray")
-//    static let byzantium = Color("Byzantium")
-//    static let rhythm = Color("Rhythm")
-//    static let redCustomized = Color("RedCustomized")
-//    static let orchidPink = Color("OrchidPink")
-//    static let ghostWhite = Color("GhostWhite")
+//    static let lightPurple = Color("LightPurple")
 }
